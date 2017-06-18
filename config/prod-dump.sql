6ca03da62334a5cfe593ec0ee805d18a913185e6
jcaffrey <jcaffrey@college.harvard.edu>
2017-06-17 22:45:59 -0400

    added simple production sql dump file

---
+++ config/prod-dump.sql
@@ -0,0 +1,4 @@
+insert into
+    pts (name, hash, email, isAdmin, createdAt, updatedAt)
+values
+    ('Prompt Admin', '$2a$08$2yDwkwaNfQIK3yD9Hyc72upxGR3eliOfC3OYvHsvtJoDUYOfRpWSe', 'prompttherapysolutions@gmail.com', true, now(), now());
