8120fa73c73efc5d75edfaa9ffb9e266bf13a531
Josh Seides <me@joshseides.com>
2017-06-26 13:34:30 -0400

    change admin dump privileges

--- config/dev-db-dump.sql
+++ config/dev-db-dump.sql
@@ -21,9 +21,9 @@ insert into
     pts (name, hash, phoneNumber, phoneProvider, email, isAdmin, createdAt, updatedAt)
 values
     ('Jeremy Welborn', '$2a$08$dtV592jmtL7UM1O0sacUGe57ndCFlAeXUH/wXaP0FE1DmJ62EWPti', '16174627953', 'att', 'jeremy@gmail.com', false, now(), now()),
-    ('Joey Caffrey', '$2a$08$2yDwkwaNfQIK3yD9Hyc72upxGR3eliOfC3OYvHsvtJoDUYOfRpWSe', '12017254565', 'att', 'joey@gmail.com', false, now(), now()),
+    ('Joey Caffrey', '$2a$08$2yDwkwaNfQIK3yD9Hyc72upxGR3eliOfC3OYvHsvtJoDUYOfRpWSe', '12017254565', 'att', 'joey@gmail.com', true, now(), now()),
     -- temp admin, change if schema changes
-    ('David Malan', '$2a$08$KyePVbpTFRdPaDcc1xAtOOCacEh6X.e.6Ud0Z/AKLJHsMHNYkqKku', '1234567890', 'att', 'david@gmail.com', true, now(), now()),
+    ('David Malan', '$2a$08$KyePVbpTFRdPaDcc1xAtOOCacEh6X.e.6Ud0Z/AKLJHsMHNYkqKku', '1234567890', 'att', 'david@gmail.com', false, now(), now()),
     ('Josh Seides', '$2a$08$tfLDCj0ypAzW20TxF4B7N.hqUhzmdYBUk5.RsE3QRbiAZVvh51Pa.', '1234567892', 'att', 'josh6810@gmail.com', true, now(), now());
 
 -- patients
