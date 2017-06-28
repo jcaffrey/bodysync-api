dbf543a32ac57e47010ed7d9dad71e4378f868ac
jcaffrey <jcaffrey@college.harvard.edu>
2017-06-28 09:17:42 -0400

    added console logs

--- models/index.js
+++ models/index.js
@@ -11,6 +11,10 @@ var db        = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
+  console.log('IN INDEX...');
+  console.log(config.database);
+  console.log(config.username);
+  console.log(config.password);
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
